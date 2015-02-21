848d139c81b7b38bd62df53bf805b424e8e69f30
Edwin Chung <tung401@yahoo.com>
2015-02-21 15:13:41 -0800

    HW 2

--- cachematrix.R
+++ cachematrix.R
@@ -2,16 +2,56 @@
 ## functions do
 
 ## Write a short comment describing this function
-
-##Edwin debug
-
+# create a matrix
 makeCacheMatrix <- function(x = matrix()) {
+        #init im to NULL (empty it)
+        im <- NULL
 
+        #set <- function(y) {
+                #x <<- y
+                #im <<- NULL
+        #}
+        
+        #This function returns entered matrix
+        get <- function() x
+        
+        #This function sets inverse matrix 
+        setim <- function(InverseM) im <<- InverseM 
+        
+        #This function gets inverse matrix
+        getim <- function() im
+        
+        #Put functions into a list
+        list(get = get,
+             setim = setim,
+             getim = getim)     
 }
 
 
 ## Write a short comment describing this function
-
+# calc inverse matrix of x. Get it from cache if it exists
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        #get inverse matrix from cache
+        im <- x$getim()
+
+        #return inverse matrix from cache if it exists
+        if(!is.null(im)) {
+                message("getting cached data")
+                return(im)
+        }
+        
+        #following code will only be executed if inverse matrix is not in cache
+        #get matrix x
+        data <- x$get()
+        
+        #calc inverse matrix
+        im <- solve(data)
+        
+        #set inverse matrix
+        x$setim(im)
+        
+        #print inverse matrix
+        im
 }
+
